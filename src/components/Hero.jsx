01bbf4ff6c63ea47bdf4478ff80fc89572523fd6
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-04-29 11:15:14 +0200

    margin top in hero test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
       className="fixed top-0 left-0 w-full h-screen z-10 flex flex-col justify-center items-center text-center px-4 sm:px-6 md:px-8 bg-[var(--accent)] text-[var(--text)] font-inter"
       id="hero"
     >
-      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full max-w-screen-xl mx-auto lg:px-20">
+      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full max-w-screen-xl mx-auto lg:px-20 mt-16">
         <div className="md:w-1/2 flex flex-col justify-center items-center md:items-start text-center md:text-left">
           <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold mb-6">
             Explore the World
