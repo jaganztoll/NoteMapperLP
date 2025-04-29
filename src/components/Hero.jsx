e571ee8dcbb9ba01d7e183a0a4e7a0b21688c6ce
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-04-29 12:12:43 +0200

    hero mobil test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ import { FaApple } from "react-icons/fa6";
 function Hero() {
   return (
     <section
-      className="fixed top-0 left-0 w-full h-screen z-10 flex flex-col justify-center items-center text-center px-4 sm:px-6 md:px-8 bg-[var(--accent)] text-[var(--text)] font-inter"
+      className="fixed top-0 left-0 w-full min-h-screen z-10 flex flex-col justify-center items-center text-center px-4 sm:px-6 md:px-8 bg-[var(--accent)] text-[var(--text)] font-inter overflow-hidden"
       id="hero"
     >
-      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full max-w-screen-xl mx-auto lg:px-20 mt-16">
+      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full max-w-screen-xl mx-auto lg:px-20 mt-16 sm:mt-8">
         <div className="md:w-1/2 flex flex-col justify-center items-center md:items-start text-center md:text-left">
           <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold mb-6">
             Explore the World
@@ -32,7 +32,7 @@ function Hero() {
           <img
             src={Device}
             alt="Device"
-            className="w-64 h-128 sm:w-76 sm:h-152 md:w-80 md:h-160 transform md:rotate-[15deg] transition-all duration-500"
+            className="w-64 h-128 sm:w-76 sm:h-152 md:w-80 md:h-160 transform sm:rotate-[10deg] md:rotate-[15deg] transition-all duration-500"
           />
         </div>
       </div>
